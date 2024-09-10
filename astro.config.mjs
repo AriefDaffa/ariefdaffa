bb30bc73808a3638fef460ac3b57d17f86956cea
AriefDaffa <ariefkeren76@yahoo.com>
2024-09-10 12:48:12 +0700

    chore: update vercel import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel(),
-});
+})
