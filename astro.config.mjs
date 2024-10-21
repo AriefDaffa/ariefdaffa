de5d7cec08dd6baefdbd5e7c1b4ab8606a5b004e
AriefDaffa <ariefkeren76@yahoo.com>
2024-10-21 10:34:58 +0700

    chore: use static site generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
 
-import vercel from '@astrojs/vercel/serverless'
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  adapter: vercelStatic(),
 })
